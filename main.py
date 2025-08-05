f3d7806052beab55e20384dda4923833f6c703c3
Marco Kade <marco.kade@ts.berlin>
2025-08-05 15:14:44 +0200

    Bisschen Code kommentiert

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
+# Alle benötigten Imports
 from gpiozero import Button, LED
 from time import sleep
 import cups
 
+
+# Setup der Umgebung & Pins
 button = Button(2)
 led = LED(17)
 
 conn = cups.Connection()
 printer_name = 'MFCL2710DN'
 
+
+# 3mal Blinken der LED, damit man weiß dass es betriebsbereit ist
 for x in range(0, 3):
     led.on()
     sleep(0.5)
     led.off()
 
+# Endlosschleife wartet auf Betätigung des Buttons
 while True:
     button.wait_for_press()
 
-    conn.printFile(printer_name, '/home/pi/eiserne-lappen/white.png', ' ', {}))
+    # Druckt leeres PNG und gibt auf der Konsole eine Rückmeldung
+    conn.printFile(printer_name, '/home/pi/eiserne-lappen/white.png', ' ', {})
     print('Push, Push!')
+
+    # LED gibt Rückmeldung, damit man weiß dass das Script noch ordnungsgemäß läuft
     led.on()
     sleep(1)
     led.off()
